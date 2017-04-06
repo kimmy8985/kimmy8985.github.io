fbf154f062f18dbb2c687f8c66ffd2dee3fd3eab
unknown <984309551@qq.com>
2017-04-06 17:41:23 +0800

    ko

--- WebQQ/js/music.js
+++ WebQQ/js/music.js
@@ -45,19 +45,19 @@ function Music(){
 			img:'images/dzq.jpg',
 			song:'喜欢你',
 			name:'邓紫棋',
-			zj:'妙味课堂'
+			zj:'一个人的天空'
 		},
 		{
 			img:'images/ps.jpg',
 			song:'平凡之路',
 			name:'朴树',
-			zj:'妙味课堂'
+			zj:'一个人的天空'
 		},
 		{
 			img:'images/zl.jpg',
 			song:'逆流成河',
 			name:'紫菱',
-			zj:'妙味课堂'
+			zj:'一个人的天空'
 		}
 	];
 	this.ylBtn = $('.yl');
